160b47da77db231f5dc051fe27d678e59280bc21
Mark St.Godard <markstgodard@gmail.com>
2016-10-16 19:15:57 -0500

    cleanup

--- scripts/deploy-store.sh
+++ scripts/deploy-store.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 source $(pwd)/scripts/cf.cfg
-source $(pwd)/scripts/cf.sh
 
 cf push ${STORE_NAME} -o markstgodard/stuff:v0 --no-start --no-manifest
-cf_setenv ${STORE_NAME} stuff v1 8080
+cf set-env ${STORE_NAME} A8_SERVICE "stuff:v0"
+cf set-env ${STORE_NAME} A8_ENDPOINT_PORT "${port}"
+cf set-env ${STORE_NAME} A8_ENDPOINT_TYPE "http"
+cf set-env ${STORE_NAME} A8_PROXY "true"
+cf set-env ${STORE_NAME} A8_REGISTER "true"
+cf set-env ${STORE_NAME} A8_REGISTRY_URL "http://${REGISTRY_NAME}.${ROUTES_DOMAIN}"
+cf set-env ${STORE_NAME} A8_CONTROLLER_URL "http://${CONTROLLER_NAME}.${ROUTES_DOMAIN}"
 
 cf start ${STORE_NAME}
